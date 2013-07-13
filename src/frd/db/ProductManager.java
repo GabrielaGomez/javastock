0d493588e53706955579f6b147cd792450ec70f7
LSI.PC3 <LSI.PC3@LSI-PC3-07>
2013-07-13 15:04:31 -0300

    Modificacion 2 de ProductManager

--- src/frd/db/ProductManager.java
+++ src/frd/db/ProductManager.java
@@ -7,30 +7,30 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
-import frd.model.User;
+import frd.model.Product;
 
 public class ProductManager extends JDBCManager {
 	public static void createDbProductTable() throws SQLException {
 		String createTableSQL = "CREATE TABLE DBPRODUCT("
 				+ "PRODUCT_ID NUMERIC(5) NOT NULL, "
 				+ "PRODUCTNAME VARCHAR(20) NOT NULL, "
-				+ "DESCRIPCION VARCHAR(20) NOT NULL "
+				+ "DESCRIPTION VARCHAR(20) NOT NULL "
 				+ "PRIMARY KEY (PRODUCT_ID) "
 				+ ")";
 
 		execute( createTableSQL );
 	}
 	
-	public static void insertProduct(int productId, String productname, String creator, Date creation) throws SQLException{
+	public static void insertProduct(int productId, String productname, String description, Date creation) throws SQLException{
 		String insertTableSQL = "INSERT INTO DBPRODUCT"
-			+ "(PRODUCT_ID, PRODUCTNAME, CREATED_BY, CREATED_DATE) " + "VALUES"
-			+ "("+productId+", '"+productname+"', '"+creator+"', " + "to_date('"
+			+ "(PRODUCT_ID, PRODUCTNAME, DESCRIPTION) " + "VALUES"
+			+ "("+productId+", '"+productname+"', '"+description+"', " + "to_date('"
 			+ dateFormat.format(creation.getTime()) + "', 'yyyy/mm/dd hh24:mi:ss'))";
 		
 		executeUpdate( insertTableSQL );
 	}
 	
-	public static void updateProduct(int productId, String productname, String creator, Date creation) throws SQLException{
+	public static void updateProduct(int productId, String productname, String description) throws SQLException{
 		String updateTableSQL = "UPDATE DBPRODUCT"
 			+ " SET PRODUCTNAME = '"+productname+"' "
 			+ " WHERE PRODUCT_ID = "+productId;
@@ -43,31 +43,28 @@ public class ProductManager extends JDBCManager {
 		
 		execute( deleteTableSQL );
 	}
-	/*
+	
 	public static List<Product> getProducts() throws SQLException{
 		List<Product> result = new ArrayList<Product>();
 		
 		String selectTableSQL = "SELECT * from DBPRODUCT";
 		
 		for( HashMap<String,Object> register : executeQuery( selectTableSQL ) ){
-			//Creo el usuario a partir de los datos obtenidos de la base
-			User usr = new User();
+			//Creo el producto a partir de los datos obtenidos de la base
+			Product usr = new Product();
 
-			if( register.containsKey("user_id") )
-				usr.setId( ((BigDecimal) register.get("user_id")).intValue() );
+			if( register.containsKey("product_id") )
+				usr.setId( ((BigDecimal) register.get("product_id")).intValue() );
 			
-			if( register.containsKey("username") )
-				usr.setUsername((String) register.get("username") );
+			if( register.containsKey("productname") )
+				usr.setName((String) register.get("productname") );
 			
-			if( register.containsKey("created_by") )
-				usr.setCreatedBy((String) register.get("created_by") );
+			if( register.containsKey("description") )
+				usr.setDescription((String) register.get("description") );
 			
-			if( register.containsKey("create_date") )
-				usr.setCreateDate((Date) register.get("create_date") );
-
 			result.add( usr );
 		}
 		
 		return result;
-	}*/
+	}
 }
